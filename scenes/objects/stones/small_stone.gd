7e336d2b7b13b0c19264973534e831f5a310e02d
Nicole Wolf <nicole.wolf@8mylez.com>
2026-05-24 22:39:34 +0200

    new

--- scenes/objects/stones/small_stone.gd
+++ scenes/objects/stones/small_stone.gd
@@ -20,13 +20,12 @@ func on_hurt(hit_damage: int) -> void:
 	material.set_shader_parameter("shake_intensity", 0.0)
 	
 func on_max_damage_reached() -> void:
-	print("max damaged reached")
 	call_deferred("add_log_scence")
 	queue_free()
 
 func add_log_scence() -> void:
 	var stone_scene = stone_scene.instantiate() as Node2D
-	stone_scene.global_position.y = global_position.y 
+	stone_scene.global_position.y = global_position.y
 	stone_scene.global_position.x = global_position.x
 	
 	get_parent().add_child(stone_scene)
